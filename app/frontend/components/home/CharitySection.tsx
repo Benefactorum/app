336597e8ce0f6df9359be629c9a8d926f668cf2f
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-11-08 09:25:02 +0100

    Causes Carousel

--- app/frontend/components/home/CharitySection.tsx
+++ app/frontend/components/home/CharitySection.tsx
@@ -1,13 +1,15 @@
 import CharityCarousel from "./CharityCarousel";
+import CauseCarousel from "./CauseCarousel";
 
 export default function CharitySection() {
   return (
-    <div className="bg-white py-4 overflow-x-hidden">
+    <div className="bg-white py-16 overflow-x-hidden">
       <CharityCarousel />
-      <div className="2xl:container mx-auto flex flex-col gap-8">
-        <h2 className="p-16 text-2xl md:text-3xl font-semibold">
+      <div className="2xl:container mx-auto flex flex-col">
+        <h2 className="px-4 my-16 text-xl sm:text-2xl md:text-3xl font-semibold">
           Soutenez les causes qui vous tiennent à coeur
         </h2>
+        <CauseCarousel />
       </div>
     </div>
   );
