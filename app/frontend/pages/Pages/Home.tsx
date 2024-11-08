c0ea4bb21c10fc4eaeb002e88aea92426ccee87a
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-11-08 18:34:24 +0100

    renaming folder for consistency

--- app/frontend/pages/Pages/Home.tsx
+++ app/frontend/pages/Pages/Home.tsx
@@ -1,8 +1,8 @@
-import HeroSection from "../../components/home/HeroSection";
-import CharitySection from "../../components/home/CharitySection";
-import JoinUsSection from "../../components/home/JoinUsSection";
-import FaqSection from "../../components/home/FaqSection";
-import QuoteSection from "../../components/home/QuoteSection";
+import HeroSection from "../../components/homepage/HeroSection";
+import CharitySection from "../../components/homepage/CharitySection";
+import JoinUsSection from "../../components/homepage/JoinUsSection";
+import FaqSection from "../../components/homepage/FaqSection";
+import QuoteSection from "../../components/homepage/QuoteSection";
 
 export default function Home() {
   return (
