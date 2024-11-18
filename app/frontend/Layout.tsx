26ed6b817d681d28f428192250eb9d57a6de8712
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-11-18 12:09:32 +0100

    Better flash design

--- app/frontend/Layout.tsx
+++ app/frontend/Layout.tsx
@@ -1,7 +1,9 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { usePage } from "@inertiajs/react";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 
 type LayoutProps = {
   children: ReactElement;
@@ -11,17 +13,21 @@ export default function Layout({ children }: LayoutProps) {
   const { flash = {} } = usePage<{
     flash?: { alert?: string; notice?: string };
   }>().props;
+
+  useEffect(() => {
+    if (flash?.notice) {
+      toast.success(flash.notice);
+    }
+    if (flash?.alert) {
+      toast.error(flash.alert);
+    }
+  }, [flash?.notice, flash?.alert]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex flex-col flex-grow">
-        {flash?.notice && (
-          <p className="py-2 px-4 bg-green-50 mb-5 text-green-500 font-medium rounded-lg inline-block">
-            {flash.notice}
-          </p>
-        )}
-        {children}
-      </main>
+      <Toaster theme="light" position="top-right" richColors closeButton />
+      <main className="flex flex-col flex-grow">{children}</main>
       <Footer />
     </div>
   );
