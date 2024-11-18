73e951f8e34946c8ae6bd261526b71bd12448ca7
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-11-18 11:18:06 +0100

    add working flash system  to layout

--- app/frontend/Layout.tsx
+++ app/frontend/Layout.tsx
@@ -1,19 +1,22 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import { ReactNode } from "react";
+import { ReactElement } from "react";
+import { usePage } from "@inertiajs/react";
 
 type LayoutProps = {
-  children: ReactNode;
-  flash?: { notice: string };
+  children: ReactElement;
 };
 
-export default function Layout({ children, flash }: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
+  const { flash = {} } = usePage<{
+    flash?: { alert?: string; notice?: string };
+  }>().props;
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-col flex-grow">
         {flash?.notice && (
-          <p className="py-2 px-3 bg-green-50 mb-5 text-green-500 font-medium rounded-lg inline-block">
+          <p className="py-2 px-4 bg-green-50 mb-5 text-green-500 font-medium rounded-lg inline-block">
             {flash.notice}
           </p>
         )}
