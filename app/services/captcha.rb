369a3c7f2550fa6c500e5972097863442055d50d
Camille Pressensé <camillefasani@gmail.com>
2024-12-03 15:27:23 +0000

    Devcontainer setup for easy collaboration. Avoid using rails secrets in dev env by bypassing CAPTCHA in development

--- app/services/captcha.rb
+++ app/services/captcha.rb
@@ -1,11 +1,13 @@
 class Captcha
-  SECRET_KEY = Rails.application.credentials.recaptcha[:secret_key]
+  SECRET_KEY = Rails.application.credentials.dig(:recaptcha, :secret_key)
 
   def initialize(token)
     @token = token
   end
 
   def valid?
+    return true if Rails.env.development?
+
     response = Net::HTTP.post_form(
       URI.parse("https://www.google.com/recaptcha/api/siteverify"),
       {
@@ -15,5 +17,8 @@ class Captcha
     )
     result = JSON.parse(response.body)
     !!result["success"]
+  rescue => e
+    Rails.logger.error "Captcha validation error: #{e.message}"
+    false
   end
 end
