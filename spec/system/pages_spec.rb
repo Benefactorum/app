c891a6c029819a8565c30ad1349b0339f4d01c39
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-06 13:45:59 +0000

    Add e2e smoke tests

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,14 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Pages", type: :system do
-  # before do
-  #   driven_by(:rack_test)
-  # end
-
-  describe "GET /" do
-    it "displays the home page" do
+  describe "#home" do
+    it "displays page" do
       visit root_path
       expect(page.text.squish).to include("Vous pouvez faire la différence, ici et maintenant")
+    end
+  end
+
+  describe "#about_us" do
+    it "displays page" do
+      visit about_us_path
+      expect(page.text.squish).to include("Vous prenez soin des autres ? On prend soin de vous !")
+    end
+  end
+
+  describe "#cofounders" do
+    it "displays page" do
+      visit cofounders_path
+      expect(page.text.squish).to include("Notre équipe")
+    end
+  end
+
+  describe "#join_us" do
+    it "displays page" do
+      visit join_us_path
+      expect(page.text.squish).to include("Nous rejoindre")
     end
   end
 end
