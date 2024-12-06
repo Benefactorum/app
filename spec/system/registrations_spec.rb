f3c279134327da1ed9733df83e00f479151f9e34
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-06 13:45:59 +0000

    Add e2e smoke tests

---
+++ spec/system/registrations_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Registrations", type: :system do
+  before do
+    # avoid redirection when hitting new_registration_path
+    # as it sets up email in the local Storage
+    visit new_connection_path
+    fill_in "Votre adresse email", with: "placeholder@email.com"
+    click_button "Continuer"
+  end
+
+  describe "#new" do
+    it "displays page" do
+      # visit new_registration_path
+      expect(page).to have_content("Créez votre compte !")
+    end
+  end
+end
