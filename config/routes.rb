b76658957c661f39846e4937ca4320ded81441e0
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-09 10:04:28 +0100

    Refactor contributions and profile pictures controllers into users namespace for better organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Authentication Routes
   get "connexion", to: "connections#new", as: :new_connection
   resources :connections, only: [:create] do
-    collection do
-      post :resend_otp
-    end
+    post :resend_otp, on: :collection
   end
-
   get "s-inscrire", to: "registrations#new", as: :new_registration
   resources :registrations, only: [:create]
-
   get "se-connecter", to: "sessions#new", as: :new_session
   resources :sessions, only: [:create, :destroy]
 
+  # User Profile and Contributions
   get "utilisateurs/:id", to: "users#show", as: :user
   get "mon-profil", to: "users#show", as: :my_profile
   resources :users, only: [:update] do
-    resource :profile_picture, only: [:update, :destroy]
-
-    resources :contributions, shallow: true
+    resource :profile_picture, only: [:update, :destroy], module: :users
+    resources :contributions, shallow: true, module: :users
   end
-  get "mes-contributions", to: "contributions#index", as: :my_contributions
-  get "mes-contributions/ajouter-une-association", to: "contributions#new", as: :my_new_contribution
+  get "mes-contributions", to: "users/contributions#index", as: :my_contributions
+  get "mes-contributions/ajouter-une-association", to: "users/contributions#new", as: :my_new_contribution
 
   # resource  :password, only: [ :edit, :update ]
   # namespace :identity do
@@ -31,10 +28,9 @@ Rails.application.routes.draw do
   #   resource :password_reset,     only: [ :new, :edit, :create, :update ]
   # end
 
+  # Static Pages
   root "pages#home"
-
   get "inertia-example", to: "inertia_example#index"
-
   get "qui-nous-sommes", to: "pages#about_us", as: :about_us
   get "co-fondateurs", to: "pages#cofounders", as: :cofounders
   get "nous-rejoindre", to: "pages#join_us", as: :join_us
@@ -42,7 +38,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # is used by Kamal Deploy
-  get "up" => "rails/health#show", :as => :rails_health_check
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
