07dc14ce10c0ef12aa3d91d6646255150ee05c6e
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-13 09:07:42 +0000

    Add production and staging deployment workflows; update environment configurations

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.headers = {"cache-control" => "public, max-age=#{1.year.to_i}"}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "https://prod.benefactorum.org"
+  config.asset_host = "https://staging.benefactorum.org"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :scaleway
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = {host: "prod.benefactorum.org"}
+  config.action_mailer.default_url_options = {host: "staging.benefactorum.org"}
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
