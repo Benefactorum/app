b9ee62a21edc7c57f5c3cdc76750852776593ab8
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-18 15:39:48 +0000

    Refactor ConnectionForm and SignUpForm to use useFormHandler hook; remove unused useConnectionForm and useSignUpForm hooks

--- app/frontend/components/forms/SignUpForm.tsx
+++ app/frontend/components/forms/SignUpForm.tsx
@@ -1,17 +1,59 @@
-import { ReactElement } from 'react'
-import { SignUpData, useSignUpForm } from '@/hooks/useSignUpForm'
+import { useEffect, useRef, ReactElement, FormEvent } from 'react'
+import { router, Link } from '@inertiajs/react'
+import { useFormHandler } from '@/hooks/useFormHandler'
 import { KeysWithStringValues } from '@/types/utilityTypes'
 import { MyInput } from './MyInput'
 import { MyCheckbox } from './MyCheckbox'
-import { Link } from '@inertiajs/react'
 import ReCAPTCHA from 'react-google-recaptcha'
 import { Button } from '@/components/ui/button'
 import { StepForward } from 'lucide-react'
 
-export function SignUpForm (): ReactElement {
-  const { data, errors, processing, signUpBlocked, recaptchaRef, updateField, submit } = useSignUpForm()
+interface SignUpData {
+  email: string
+  first_name: string
+  last_name: string
+  accepts_conditions: boolean
+  recaptcha_token: string
+}
 
-  function renderInput (id: KeysWithStringValues<SignUpData>, label: string, placeholder: string): ReactElement {
+export function SignUpForm (): ReactElement {
+  const {
+    data,
+    processing,
+    updateField,
+    submit
+  } = useFormHandler<SignUpData>({
+    initialData: {
+      email: sessionStorage.getItem('email') ?? '',
+      first_name: sessionStorage.getItem('firstName') ?? '',
+      last_name: sessionStorage.getItem('lastName') ?? '',
+      accepts_conditions: Boolean(sessionStorage.getItem('acceptsConditions')),
+      recaptcha_token: ''
+    },
+    postUrl: '/signup'
+  })
+
+  const recaptchaRef = useRef<ReCAPTCHA>(null)
+  const signUpBlocked = Boolean(sessionStorage.getItem('signUpBlocked'))
+
+  useEffect(() => {
+    if (data.email === '') {
+      router.get('/connexion')
+    }
+  }, [data.email])
+
+  async function submit2 (e: FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault()
+    const token = (recaptchaRef.current != null) ? await recaptchaRef.current.executeAsync() as string : ''
+    data.recaptcha_token = token
+    submit()
+  }
+
+  function renderInput (
+    id: KeysWithStringValues<SignUpData>,
+    label: string,
+    placeholder: string
+  ): ReactElement {
     return (
       <MyInput
         id={id}
@@ -22,23 +64,21 @@ export function SignUpForm (): ReactElement {
         value={data[id]}
         onChange={e => updateField(id, e.target.value)}
         placeholder={placeholder}
-        error={errors[id]}
       />
     )
   }
 
   return (
-    <form onSubmit={(e) => { void submit(e) }} className='w-full flex flex-col pt-4 gap-8'>
+    <form onSubmit={e => { void submit2(e) }} className='w-full flex flex-col pt-4 gap-8'>
       {renderInput('first_name', 'Prénom :', 'Alain')}
       {renderInput('last_name', 'Nom :', 'Connu')}
 
       <MyCheckbox
-        id='terms'
+        id='accepts_conditions'
         required
         disabled={signUpBlocked}
         checked={data.accepts_conditions}
         onCheckedChange={checked => updateField('accepts_conditions', checked)}
-        error={errors.terms_and_privacy_accepted_at}
       >
         Je confirme avoir lu et accepté les{' '}
         <Link
