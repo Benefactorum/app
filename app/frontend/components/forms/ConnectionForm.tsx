4a11ea2c3fd3513b0e4855d3819af076a752dd73
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-18 12:27:39 +0000

    Refactor ConnectionForm to use MyInput component and enhance error handling; remove unused test-utils file

--- app/frontend/components/forms/ConnectionForm.tsx
+++ app/frontend/components/forms/ConnectionForm.tsx
@@ -1,38 +1,29 @@
 import { ReactElement } from 'react'
 import { useConnectionForm } from '@/hooks/useConnectionForm'
-import { Input } from '@/components/ui/input'
+import { MyInput } from './MyInput'
 import { Button } from '@/components/ui/button'
-import { InputError } from '@/components/forms/InputError'
 import { StepForward } from 'lucide-react'
 
 export function ConnectionForm (): ReactElement {
-  const { data, errors, processing, updateEmail, validateAndSubmit } =
-    useConnectionForm()
+  const { data, errors, processing, updateEmail, validateAndSubmit } = useConnectionForm()
 
   return (
     <form
-      onSubmit={(e) => validateAndSubmit(e)}
-      className='w-full flex flex-col pt-4 gap-8'
+      onSubmit={e => validateAndSubmit(e)}
+      className='w-full flex flex-col gap-8'
       aria-label='form'
     >
-      <div>
-        <Input
-          type='email'
-          required
-          autoFocus
-          title='Votre adresse email'
-          placeholder='Votre adresse email'
-          value={data.email}
-          onChange={(e) => updateEmail(e.target.value)}
-          className={
-            'bg-white focus-visible:ring-0 focus-visible:border-primary placeholder:text-ellipsis placeholder:text-xs md:placeholder:text-sm focus-visible:ring-offset-0' +
-            (typeof errors.email === 'string' && errors.email !== '' ? ' border-red-600' : '')
-          }
-        />
-        {typeof errors.email === 'string' && errors.email !== '' && <InputError>{errors.email}</InputError>}
-      </div>
+      <MyInput
+        type='email'
+        required
+        autoFocus
+        value={data.email}
+        onChange={e => updateEmail(e.target.value)}
+        placeholder='Votre adresse email'
+        error={errors.email}
+      />
 
-      <Button type='submit' disabled={processing || errors.email}>
+      <Button type='submit' disabled={processing || Boolean(errors.email)}>
         <StepForward />
         Continuer
       </Button>
