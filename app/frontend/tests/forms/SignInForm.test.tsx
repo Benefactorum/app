fe0bcd163a3fb33dba0cc477b36eb8c4154238a6
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-12-20 10:10:37 +0000

    add tests

---
+++ app/frontend/tests/forms/SignInForm.test.tsx
@@ -0,0 +1,120 @@
+import { describe, it, expect, vi, beforeEach, beforeAll, Mock } from 'vitest'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import SignInForm from '@/components/forms/SignInForm'
+import { useFormHandler } from '@/hooks/useFormHandler'
+import { router } from '@inertiajs/react'
+
+vi.mock('@inertiajs/react', async () => {
+  const actual = await vi.importActual('@inertiajs/react')
+  return {
+    ...actual,
+    router: {
+      get: vi.fn(),
+      post: vi.fn()
+    }
+  }
+})
+
+vi.mock('sonner', () => ({
+  toast: {
+    success: vi.fn()
+  }
+}))
+
+vi.mock('@/hooks/useFormHandler', () => ({
+  useFormHandler: vi.fn(() => ({
+    data: {
+      email: 'test@example.com',
+      code: ''
+    },
+    updateField: vi.fn(),
+    submit: vi.fn((e: Event) => e.preventDefault()),
+    processing: false,
+    errors: {}
+  }))
+}))
+
+describe('SignInForm', () => {
+  let mockedUpdateField: Mock
+  let mockedSubmit: Mock
+
+  beforeEach(() => {
+    const useFormHandlerMock = vi.mocked(useFormHandler)
+    mockedUpdateField = vi.fn()
+    mockedSubmit = vi.fn((e) => e.preventDefault())
+
+    useFormHandlerMock.mockReturnValue({
+      data: {
+        email: 'test@example.com',
+        code: ''
+      },
+      updateField: mockedUpdateField,
+      submit: mockedSubmit,
+      processing: false,
+      errors: {}
+    })
+
+    vi.clearAllMocks()
+  })
+
+  it('renders the OTP input and submit button', () => {
+    render(<SignInForm />)
+
+    const inputOTP = screen.getByRole('textbox')
+    expect(inputOTP).toBeInTheDocument()
+
+    const button = screen.getByRole('button', { name: /Continuer/i })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('calls updateField on OTP input change', () => {
+    render(<SignInForm />)
+    const slots = screen.getAllByRole('textbox')
+    fireEvent.change(slots[0], { target: { value: '1' } })
+    expect(mockedUpdateField).toHaveBeenCalledWith('code', '1')
+  })
+
+  it('submits the form when "Continuer" is clicked', () => {
+    const { container } = render(<SignInForm />)
+    const form = container.querySelector('form')
+    expect(form).not.toBeNull()
+    if (form === null) throw new Error('Form not found')
+    fireEvent.submit(form)
+    expect(mockedSubmit).toHaveBeenCalled()
+  })
+
+  it('disables the button if processing or there is an error', () => {
+    const useFormHandlerMock = vi.mocked(useFormHandler)
+    useFormHandlerMock.mockReturnValueOnce({
+      data: {
+        email: 'test@example.com',
+        code: ''
+      },
+      updateField: mockedUpdateField,
+      submit: mockedSubmit,
+      processing: true,
+      errors: {}
+    })
+
+    render(<SignInForm />)
+    const button = screen.getByRole('button', { name: /Continuer/i })
+    expect(button).toBeDisabled()
+  })
+
+  it('navigates back if no email is found', () => {
+    const useFormHandlerMock = vi.mocked(useFormHandler)
+    useFormHandlerMock.mockReturnValueOnce({
+      data: {
+        email: '',
+        code: ''
+      },
+      updateField: mockedUpdateField,
+      submit: mockedSubmit,
+      processing: false,
+      errors: {}
+    })
+
+    render(<SignInForm />)
+    expect(router.get).toHaveBeenCalledWith('/connexion')
+  })
+})
