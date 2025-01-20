f341a18bfb58f5e7322b1c022dbacd02a1566a5b
Maximilien Pressensé <maximilien.pressense@hey.com>
2025-01-20 15:58:51 +0000

    Refactor intervention area and keyword creation handling
    - Removed error handling for ActiveRecord::RecordInvalid in the create actions of InterventionAreasController and KeywordsController to simplify the response structure.
    - Added length validation (3 to 30 characters) for the name attribute in both InterventionArea and Keyword models to enforce stricter input requirements.
    - Enhanced the MyAsyncCreatableSelect component with user feedback for creation errors and improved validation for new options.
    - Updated the French locale file to include translations for intervention areas and keywords.
    
    This commit streamlines the creation process for intervention areas and keywords, ensuring better validation and user experience.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,7 +2,7 @@ class Keyword < ApplicationRecord
   has_many :osbls_keywords, dependent: :destroy # join table
   has_many :osbls, through: :osbls_keywords
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 30}
 
   normalizes :name, with: ->(name) { name.strip.downcase.capitalize }
 end
