598074d2c4eebad493ab4b7f0626e7c61e61e6d8
Maximilien Pressensé <maximilien.pressense@hey.com>
2025-01-22 09:41:45 +0000

    Enhance annual finance management in contribution form
    - Updated the contributions_controller to include nested attributes for fund sources within annual finances.
    - Enhanced the OsblFinance component to allow users to add, remove, and manage fund sources, including type, percent, and amount.
    - Modified the FormData type to include fund_sources_attributes for better data handling.
    - Updated the AnnualFinance model to accept nested attributes for fund sources, improving data integrity and user experience.
    
    This commit improves the contribution form by providing comprehensive management of financial data, allowing users to specify detailed funding sources.

--- app/models/annual_finance.rb
+++ app/models/annual_finance.rb
@@ -3,4 +3,6 @@ class AnnualFinance < ApplicationRecord
 
   validates :year, numericality: {less_than_or_equal_to: Time.current.year}
   # validates :employees_count, numericality: { greater_than: 0 }, allow_nil: true
+
+  accepts_nested_attributes_for :fund_sources
 end
