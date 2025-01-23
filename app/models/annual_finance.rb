8553b67fba7e614f49a5e474b602a8d2e7b3f6b0
Maximilien Pressensé <maximilien.pressense@hey.com>
2025-01-23 10:36:13 +0000

    Enhance AnnualFinance model with validations and associations
    - Added association to OSBL in the AnnualFinance model to establish a relationship.
    - Implemented a custom validation to ensure at least one relevant piece of information is provided alongside the year.
    - Updated the OSBL factory to use sequences for name and website attributes, improving test data generation.
    - Removed outdated test factories for AnnualFinance, streamlining the test suite.
    
    This commit improves data integrity and validation in the AnnualFinance model, ensuring that necessary information is captured while enhancing the test setup.

--- app/models/annual_finance.rb
+++ app/models/annual_finance.rb
@@ -1,8 +1,22 @@
 class AnnualFinance < ApplicationRecord
+  belongs_to :osbl
   has_many :fund_sources, dependent: :destroy
 
   validates :year, numericality: {less_than_or_equal_to: Time.current.year}
   # validates :employees_count, numericality: { greater_than: 0 }, allow_nil: true
 
   accepts_nested_attributes_for :fund_sources
+
+  validate :at_least_one_information
+
+  private
+
+  def at_least_one_information
+    return if fund_sources.any?
+
+    attributes_to_check = attributes.except("id", "year", "osbl_id", "created_at", "updated_at")
+    if attributes_to_check.values.all?(&:blank?)
+      errors.add(:base, "Au moins une information est requise en plus de l'année.")
+    end
+  end
 end
