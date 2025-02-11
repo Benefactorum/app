14d5cd57d4338f2baaca435801b80195a32557b4
Maximilien Pressensé <maximilien.pressense@hey.com>
2025-02-11 15:53:11 +0000

    add new smoke test

--- spec/system/contributions_spec.rb
+++ spec/system/contributions_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe "Contributions", type: :system do
       connect_as(create(:user, :with_otp))
     end
     it "displays page" do
+      visit my_contributions_path
+      expect(page).to have_text("Mon historique")
+    end
+  end
+
+  describe "/mes-contributions/ajouter-une-association" do
+    before do
+      connect_as(create(:user, :with_otp))
+    end
+    it "displays page" do
       visit my_new_contribution_path
       expect(page).to have_text("Ajouter une association")
     end
